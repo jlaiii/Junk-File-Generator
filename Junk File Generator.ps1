c941b31aba96b17dd88d8e80fff6961dc0f0b78c
Jay <20913004+jlaiii@users.noreply.github.com>
2022-12-14 23:49:30 -0600

    Add files via upload

---
+++ Junk File Generator.ps1
@@ -0,0 +1,18 @@
+
+Write-Output "Welcome to the Junk File Generator!"
+sleep 3
+$ErrorActionPreference = 'SilentlyContinue'
+$junkFolder = "$PSScriptRoot\junk"
+New-Item -ItemType Directory -Path $junkFolder
+
+while($true) {
+    $filePath = "$junkFolder\junkfile$(Get-Random).junk"
+    $junkData = New-Object byte[] (100MB)
+    try {
+        [System.IO.File]::WriteAllBytes($filePath, $junkData)
+        Write-Output "Created junk file: $filePath"
+    } catch {
+        Write-Output "Drive Full. Test done."
+        break
+    }
+}
